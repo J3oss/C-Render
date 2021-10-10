9118b25fdf125ecfad74a6479eaefa740398a7c1
j3oss <yo_90_32@hotmail.com>
2021-10-10 18:02:40 +0200

    added scene changing and timers for benchmarks

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,16 @@
 #include <scene.h>
 #include <scene_manager.h>
 
+#include <ctime>
+#include <ratio>
+#include <chrono>
+#include <thread>
+
+#include <var.h>
+
+using namespace std::chrono;
+float bench = 0;
+uint32_t runs = 1000;
 int main()
 {
   SDL_Event event;
@@ -14,11 +24,19 @@ int main()
 
   SceneManager sceneManager;
 
-  auto headwithcameraScene = new Scene("res/headwithcamera.glb");
+  auto headwithcameraScene = new Scene("res/Head/Head.gltf");
   std::shared_ptr<Scene> pheadwithcameraScene(headwithcameraScene);
   sceneManager.AddScene(pheadwithcameraScene);
 
-  // auto cubeplaneScene = new Scene("res/cube&plane.glb");
+  auto damagedHelmetScene = new Scene("res/DamagedHelmet/DamagedHelmet.gltf");
+  std::shared_ptr<Scene> pDamagedHelmetScene(damagedHelmetScene);
+  sceneManager.AddScene(pDamagedHelmetScene);
+
+  // auto CesiumMilkTruckScene = new Scene("res/CesiumMilkTruck/CesiumMilkTruck.gltf");
+  // std::shared_ptr<Scene> pCesiumMilkTruckScene(CesiumMilkTruckScene);
+  // sceneManager.AddScene(pCesiumMilkTruckScene);
+  //
+  // auto cubeplaneScene = new Scene("res/Cube&Plane/Cube&Plane.gltf");
   // std::shared_ptr<Scene> pCubePlane(cubeplaneScene);
   // sceneManager.AddScene(pCubePlane);
 
@@ -26,24 +44,52 @@ int main()
   renderer.SetViewPort(0, 0);
 
   while( !quit )
+  // while (runs--)
   {
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
     renderer.Clear();
-    renderer.DrawScene(sceneManager.GetScene(0));
+    renderer.DrawScene(sceneManager.GetActiveScene());
     renderer.Update();
 
     while( SDL_PollEvent( &event ) )
     {
         switch( event.type )
         {
-            case SDL_QUIT:
+          case SDL_KEYUP:
+          {
+            switch( event.key.keysym.sym )
+            {
+
+              case SDLK_q:
                 quit = 1;
                 break;
+              case SDLK_n:
+                sceneManager.NextActiveScene();
+                break;
+            }
+            break;
+          }
 
-            default:
-              break;
+          case SDL_QUIT:
+          {
+            quit = 1;
+            break;
+          }
+
         }
     }
+
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+    int sleepTime = (1.0f/60.0f - time_span.count()) * 1000;
+    if(sleepTime > 0) std::this_thread::sleep_for(std::chrono::milliseconds( sleepTime ));
+
+    std::cout << "FPS(before limiting):"<< 1.0/time_span.count() << std::endl;
+    // bench+=1.0/time_span.count();
   }
 
+  // printf("avg fps: %f\n", bench/1000);
   SDL_Quit();
 }
