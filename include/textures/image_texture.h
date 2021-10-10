63cbc642ca305561259974d04549b05289f7884e
j3oss <yo_90_32@hotmail.com>
2021-10-10 17:35:06 +0200

    fixed uv mapping (remove flipping)

--- include/textures/image_texture.h
+++ include/textures/image_texture.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <textures/texture.h>
-#include <string>
 #include <image.h>
-#include <iostream>
 
 class ImageTexture : public Texture
 {
@@ -12,15 +10,13 @@ public:
 
   virtual Color value(float u, float v) const override
   {
-    u *= (float)mImage.mWidth;
-    v *= (float)mImage.mHeight;
+    u *= mImage.mWidth;
+    v *= mImage.mHeight;
 
     uint32_t FinalU = (uint32_t)u % mImage.mWidth;
     uint32_t FinalV = (uint32_t)v % mImage.mHeight;
 
-    // printf("%d %d\n", FinalU, FinalV);
-
-    return mImage.mPixels[ ((mImage.mHeight - FinalV) * mImage.mWidth) + FinalU];
+    return mImage.mPixels[ FinalV * mImage.mWidth + FinalU];
   }
 
 private:
