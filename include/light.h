4a2adf0feed1aa8370b3d43df202e20bf25a2611
j3oss <yo_90_32@hotmail.com>
2021-10-14 21:46:24 +0200

    added light class

---
+++ include/light.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ext/glm/glm/glm.hpp>
+
+#include <node.h>
+
+enum class LightType { POINT };
+
+class Light : public Node
+{
+public:
+  Light() {}
+  Light(glm::vec3 direction) {mDirection = direction;}
+
+  float mConstant;
+  float mLinear;
+  float mQuadratic;
+
+  glm::vec3 mAmbientColor;
+  glm::vec3 mDiffuseColor;
+  glm::vec3 mSpecularColor;
+
+  glm::vec3 mDirection;
+
+  LightType mType;
+};
